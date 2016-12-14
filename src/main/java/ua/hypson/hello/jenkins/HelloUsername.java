761c76fc3e7dfff287d1245f8ae24e340c123a80
vasiliy <zhar.vasiliy@gmail.com>
2016-12-14 18:53:05 +0200

    initial commit

---
+++ src/main/java/ua/hypson/hello/jenkins/HelloUsername.java
@@ -0,0 +1,11 @@
+package ua.hypson.hello.jenkins;
+
+/**
+ * @author vasiliy
+ */
+public class HelloUsername {
+
+    public String hello(String username) {
+        return "Hello, " + username;
+    }
+}
---
+++ src/test/java/ua/hypson/hello/jenkins/HelloUsernameTest.java
@@ -0,0 +1,17 @@
+package ua.hypson.hello.jenkins;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author vasiliy
+ */
+public class HelloUsernameTest {
+
+    @Test
+    public void helloTest() {
+        HelloUsername helloUsername = new HelloUsername();
+        assertEquals("Hello, John", helloUsername.hello("John"));
+    }
+}
